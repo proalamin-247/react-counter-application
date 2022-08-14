1f938354dbebebf327dcf94efdc4d87daed43d51
proalamin-247 <pro.alamin247@gmail.com>
2022-08-14 11:52:52 +0600

    useState use and counter button work prefectly

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Counter() {
-  return (
-      <div class="text-center bg-white p-4 space-y-5 bg-white rounded shadow">
-          <div>
-              <h1 class="text-2xl text-semibold" id="counter">0</h1>
-          </div>
-          <div class="space-x-2">
-              <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment">
-                  Increment
-              </button>
-              <button class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement">
-                  Decrement
-              </button>
-          </div>
-      </div>
-  )
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount((preCount) => preCount + 1);
+    };
+
+    const decrement = () => {
+        setCount((preCount) => preCount - 1);
+    };
+
+    return (
+        <div class="text-center bg-white p-4 space-y-5 bg-white rounded shadow">
+            <div>
+                <h1 class="text-2xl text-semibold" id="counter">{count}</h1>
+            </div>
+            <div class="space-x-2">
+                <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment" onClick={increment}>
+                    Increment
+                </button>
+                <button class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement" onClick={decrement}>
+                    Decrement
+                </button>
+            </div>
+        </div>
+    )
 }
